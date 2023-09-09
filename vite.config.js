f2430649799c65bcbc8bd96ca3dbd998f45e8e01
highfee <odediranifeoluwa7@gmail.com>
2023-09-09 14:13:18 +0100

    create the header

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "blog.html"),
+        nested: resolve(__dirname, "about.html"),
       },
     },
   },
