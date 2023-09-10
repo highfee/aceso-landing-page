dff815ac2a1af451215d9cd2bced03b55298fca9
highfee <odediranifeoluwa7@gmail.com>
2023-09-10 18:58:04 +0100

    added openweather map

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var map = L.map("map", { zoom: 19, scrollWheelZoom: false }).setView(
 L.tileLayer(
   "https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}",
   {
-    attribution: '&copy; <a href="http://maps">Google map</a>',
+    attribution:
+      '&copy; <a href="https://www.google.com/maps/search/+White-House+Obio-akpo+LGA+Rivers+State/@4.8782231,6.9625183,18.97z?entry=ttu">Google map</a>',
   }
 ).addTo(map);
